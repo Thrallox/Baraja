45a829d3bac01b38c57bc5db3ed3c6f271034ba4
Thrallox <javieraguilargene@gmail.com>
2020-11-11 00:08:55 +0100

    version mejorada 2

--- baraja.py
+++ baraja.py
@@ -3,11 +3,10 @@ numeros = ['A', '2', '3', '4', '5', '6', '7', 'S', 'C', 'R']
 baraja = []
 
 def creaBaraja():
+    baraja = []
     for item in palos: 
         for ix in numeros:
             baraja.append(item + ix) 
     return baraja 
-    
-española = creaBaraja()
 
-print("La española es", española)
+
